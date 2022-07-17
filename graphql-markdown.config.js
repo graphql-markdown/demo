377914b0de7add9b8776e0f4d158d3003084f460
Grégory Heitz <edno@edno.io>
2022-07-17 19:00:18 +0000

    add docOptions to config

--- graphql-markdown.config.js
+++ graphql-markdown.config.js
@@ -5,5 +5,10 @@ module.exports = {
   homepage: "static/index.md",
   loaders: {
     UrlLoader: "@graphql-tools/url-loader"
+  },
+  docOptions: {
+    pagination: false, // disable buttons previous and next, same as cli flag --noPagination
+    toc: false, // disable page table of content, same as cli flag --noToc
+    index: true, // enable generated index pages, same as cli flag --index
   }
 };
