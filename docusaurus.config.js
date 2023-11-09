2c7b7e0a4850a610cfce5f124953aeb0ca7c46a8
Grégory Heitz <324670+edno@users.noreply.github.com>
2023-11-09 11:34:33 +0000

    refresh demo

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -1,7 +1,8 @@
 // @ts-check
 // Note: type annotations allow type checking and IDEs autocompletion
-const lightCodeTheme = require("prism-react-renderer/themes/github");
-const darkCodeTheme = require("prism-react-renderer/themes/dracula");
+const { themes } = require("prism-react-renderer");
+const lightCodeTheme = themes.github,
+  darkCodeTheme = themes.dracula;
 
 /** @type {import('@docusaurus/types').Config} */
 const config = {
@@ -23,10 +24,10 @@ const config = {
         blog: false,
         docs: {
           routeBasePath: "/",
-          sidebarPath: require.resolve("./sidebars.js")
+          sidebarPath: "./sidebars.js",
         },
         theme: {
-          customCss: require.resolve("./src/css/custom.css")
+          customCss: ["./src/css/custom.css"],
         }
       })
     ]
